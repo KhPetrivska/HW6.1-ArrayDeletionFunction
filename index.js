16613f390770413e87bc14ec95bb1637337b3c5e
Kristina <kristin.petrivska@gmail.com>
2024-07-20 16:28:38 -0600

    add semicolons were needed

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 'use strict'
-const firstInput = prompt("Enter something")
-const charactersToDelete = prompt("Now enter characters that have to be removed from the previous input")
+const firstInput = prompt("Enter something");
+const charactersToDelete = prompt("Now enter characters that have to be removed from the previous input");
 
 if ((!firstInput?.trim()) || (!charactersToDelete?.trim())) {
-  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again")
+  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again");
 }else{
 const filterUserInput = function(fullInput, extractions ) {
-    let filteredInput = []
-      for (let character of fullInput){
-        if (!extractions.toLowerCase().includes(character.toLowerCase()) || character === ' ' ){
-          filteredInput.push(character)
-        }
+    let filteredInput = [];
+    for (let character of fullInput){
+      if (!extractions.toLowerCase().includes(character.toLowerCase()) || character === ' ' ){
+        filteredInput.push(character);
+      }
     }
-  return filteredInput.join('')
+  return filteredInput.join('');
 }
-
-
-console.log(filterUserInput(firstInput, charactersToDelete))
+console.log(filterUserInput(firstInput, charactersToDelete));
 }
