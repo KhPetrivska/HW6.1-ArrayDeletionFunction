580d435df4eefb21e9a8dc56ed5ab364630d5074
Kristina <kristin.petrivska@gmail.com>
2024-07-20 22:00:04 -0600

    add semicolons and dots to the code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
 const filterUserInput = function(fullInput, extractions ) {
-  let filteredInput = []
+  let filteredInput = [];
     for (let character of fullInput){
       if (!extractions.toLowerCase().includes(character.toLowerCase()) || character === ' ' ){
-        filteredInput.push(character)
+        filteredInput.push(character);
       }
   }
-return filteredInput.join('')
+return filteredInput.join('');
 }
 
-const firstInput = prompt("Enter something")
-const secondInput= prompt("Now enter characters that have to be removed from the previous input")
+const firstInput = prompt("Enter something.");
+const secondInput= prompt("Now enter characters that have to be removed from the previous input.");
 
 if ((!firstInput?.trim()) || (!secondInput?.trim())) {
-  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again")
+  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again.")
 }else{
-console.log(filterUserInput(firstInput, secondInput))
+console.log(filterUserInput(firstInput, secondInput));
 }
