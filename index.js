fb287ef1f9adaaf817311059991547ecabe28eb1
Kristina <kristin.petrivska@gmail.com>
2024-07-20 21:54:59 -0600

    change agruments titles

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict'
-const firstInput = prompt("Enter something");
-const charactersToDelete = prompt("Now enter characters that have to be removed from the previous input");
-
-if ((!firstInput?.trim()) || (!charactersToDelete?.trim())) {
-  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again");
-}else{
 const filterUserInput = function(fullInput, extractions ) {
-    let filteredInput = [];
+  let filteredInput = []
     for (let character of fullInput){
       if (!extractions.toLowerCase().includes(character.toLowerCase()) || character === ' ' ){
-        filteredInput.push(character);
+        filteredInput.push(character)
       }
-    }
-  return filteredInput.join('');
+  }
+return filteredInput.join('')
 }
-console.log(filterUserInput(firstInput, charactersToDelete));
+
+const firstInput = prompt("Enter something")
+const secondInput= prompt("Now enter characters that have to be removed from the previous input")
+
+if ((!firstInput?.trim()) || (!secondInput?.trim())) {
+  alert("At least one of the inputs was either canceled or left empty. Refresh the page and try again")
+}else{
+console.log(filterUserInput(firstInput, secondInput))
 }
