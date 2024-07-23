a44d6fe6923f61e0dcbe608f61f96244a7a297ad
Kristina <kristin.petrivska@gmail.com>
2024-07-22 23:04:47 -0600

    refactor second parameter

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 'use strict'
-const filterSelectedCharacters = function(fullInput, extractions ) {
-  let filteredInput = [];
-    for (let character of fullInput){
-      if (!extractions.toLowerCase().includes(character.toLowerCase()) || character === ' ' ){
-        filteredInput.push(character);
-      }
-  }
-return filteredInput.join('');
-}
 
 const firstInput = prompt("Enter something.");
 if (!firstInput?.trim()) {
-  alert("Your first input was either canceled or left empty. Refresh the page and try again.")
-}else{
+  alert("Your first input was either canceled or left empty. Refresh the page and try again.");
+} else {
   const secondInput= prompt("Now enter characters that have to be removed from the previous input."); 
-  if (!secondInput?.trim()){
-    alert("Your second input was either canceled or left empty. Refresh the page and try again.")
-  }else{
-    console.log(filterSelectedCharacters(firstInput, secondInput));
+  if (!secondInput?.trim()) {
+    alert("Your second input was either canceled or left empty. Refresh the page and try again.");
+  } else {
+    const secondInputArray = secondInput.toLowerCase().replace(' ','').split('')
+    const result = filterSelectedCharacters(firstInput, secondInputArray)
+    console.log(result)
   }
+}
+
+function filterSelectedCharacters (fullInput, extractions ) {
+  let filteredInput = [];
+  for (let character of fullInput) {
+        if (!extractions.includes(character.toLowerCase())) {
+          filteredInput.push(character);
+      }
+  }
+return filteredInput.join('');
 }
